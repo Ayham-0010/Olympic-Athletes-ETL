3682c242039e61ff266725915b90548dfe94f761
Ayham <ayham.zinedine01@gmail.com>
2025-11-01 13:34:11 +0300

    Refactor and modularize data cleaning pipeline for Olympic datasets; added .copy() for immutability, fixed date/height bugs, improved logging, and structured ETL functions for bios, results, and editions.

--- data_clean.py
+++ data_clean.py
@@ -1,71 +1,131 @@
 import pandas as pd
-import re
+import re, os
+
+from logger import logger
+
+
+
+def drop_invalid_columns(df, columns_to_drop):
+
+    df = df.copy()
+
+    df = df.drop(columns=columns_to_drop, errors='ignore')
+
+    return df
 
 
 # Bio data cleaning
 
-df = pd.read_csv("../raw_data/biodata.csv")
 
-df["Name"]= df["Used name"].str.replace('•',' ', regex=False)
+def name_parsing(df):
 
-df['Hight (cm)'] = pd.to_numeric(df['Measurements'].str.split('/').str[0].str.strip(' cm'), errors='coerce')
-df['Weight (kg)'] =pd.to_numeric(df['Measurements'].str.split('/').str[1].str.strip(' kg'), errors='coerce')
+    df = df.copy()
 
-date_pattern = r'(\d{1,2}\s+[A-Za-z]+\s+\d{4}|\d{4})'
+    df["Name"]= df["Used name"].str.replace('•',' ', regex=False)
+    return df
 
-df['Born_Date'] = df['Born'].str.extract(date_pattern)
-df['Born_Date'] = pd.to_datetime(df['Born_Date'], format="mixed", errors='coerce')
+def measurements_parsing(df):
+    df = df.copy()
+    df['Height (cm)'] = pd.to_numeric(df['Measurements'].str.split('/').str[0].str.strip(' cm'), errors='coerce')
+    df['Weight (kg)'] =pd.to_numeric(df['Measurements'].str.split('/').str[1].str.strip(' kg'), errors='coerce')
 
-df['Died_Date'] = df['Born'].str.extract(date_pattern)
-df['Died_Date'] = pd.to_datetime(df['Died_Date'], format="mixed", errors='coerce')
+    return df
 
-location_pattern = r"in\s+(.+?),\s*([A-Za-zÀ-ÿ'’\- ]+)\s*\((\w+)\)$"
+def date_parsing(df):
 
-df[['Born_City', 'Born_Region', 'Born_Country']] = df['Born'].str.extract(location_pattern, expand=True)
+    df = df.copy()
 
-df[['Affiliations_Club', 'Affiliations_City', 'Affiliations_Country']] = (
-    df['Affiliations']
-    .astype(str)
-    .str.strip()
-    .str.extract(r'^(.+?)(?:,\s*(.+?))?(?:\s*\((.+?)\))?$')
-)
+    date_pattern = r'(\d{1,2}\s+[A-Za-z]+\s+\d{4}|\d{4})'
 
-df['Roles'] = df['Roles'].astype(str).str.strip().replace('—', '').str.split(' • ')
+    df['Born_Date'] = df['Born'].str.extract(date_pattern)
+    df['Born_Date'] = pd.to_datetime(df['Born_Date'], format="mixed", errors='coerce')
 
-columns_to_drop = [ 'Used name', 'Born', 'Died', 'Full name', 'Measurements', 'Affiliations','Title(s)',  'Nationality', 'Other names', 'Original name', 'Name order', 'Nick/petnames']
+    df['Died_Date'] = df['Died'].str.extract(date_pattern)
+    df['Died_Date'] = pd.to_datetime(df['Died_Date'], format="mixed", errors='coerce')
+    return df
 
-df = df.drop(columns=columns_to_drop)
+def location_parsing(df):
 
-# df.to_csv('./clean_data/cleaned_biodata.csv', index=False)
+    df = df.copy()
+
+    location_pattern = r"in\s+(.+?),\s*([A-Za-zÀ-ÿ'’\- ]+)\s*\((\w+)\)$"
+
+    df[['Born_City', 'Born_Region', 'Born_Country']] = df['Born'].str.extract(location_pattern, expand=True)
+    return df
+
+def affiliations_parsing(df):
+
+    df = df.copy()
+
+    df[['Affiliations_Club', 'Affiliations_City', 'Affiliations_Country']] = (
+        df['Affiliations']
+        .astype(str)
+        .str.strip()
+        .str.extract(r'^(.+?)(?:,\s*(.+?))?(?:\s*\((.+?)\))?$')
+    )
+    return df
+
+def roles_parsing(df):
+
+    df = df.copy()
+
+    df['Roles'] = df['Roles'].astype(str).str.strip().replace('—', '').str.split(' • ')
+    return df
+
+
+
+def clean_biodata(df):
+
+    df = name_parsing(df)
+    df = measurements_parsing(df)
+    df = date_parsing(df)
+    df = location_parsing(df)
+    df = affiliations_parsing(df)
+    df = roles_parsing(df)
+
+    columns_to_drop = [ 'Used name', 'Born', 'Died', 'Full name', 'Measurements', 'Affiliations','Title(s)',  'Nationality', 'Other names', 'Original name', 'Name order', 'Nick/petnames']
+    df = drop_invalid_columns(df, columns_to_drop)
+    return df
+
 
 
 # Results data cleaning
 
-df = pd.read_csv("../raw_data/results.csv")
 
-df[['Game Year', 'Game Type']] = df['Games'].str.extract(r'^(\d{4})\s+(.*)$', expand=True)
-df['Game Year'] = pd.to_numeric(df['Game Year'])
+def game_year_type_parsing(df):
 
-df['Position'] = df['Pos'].str.extract(r'(\d+)')
-df['Position'] = pd.to_numeric(df['Position'])
+    df = df.copy()
 
-df['Tied'] = df['Pos'].str.contains('=')
+    df[['Game Year', 'Game Type']] = df['Games'].str.extract(r'^(\d{4})\s+(.*)$', expand=True)
+    df['Game Year'] = pd.to_numeric(df['Game Year'])
 
-columns_to_drop = ['Nationality', 'Unnamed: 7', 'Games', 'Pos']
-df = df.drop(columns=columns_to_drop)
+    return df
 
-# df.to_csv('./clean_data/cleaned_results.csv', index=False)
+def position_parsing(df):
+
+    df = df.copy()
+    
+    df['Position'] = df['Pos'].str.extract(r'(\d+)')
+    df['Position'] = pd.to_numeric(df['Position'])
+
+    df['Tied'] = df['Pos'].str.contains('=')
+
+    return df
+
+
+
+
+def clean_results(df):
+    df = game_year_type_parsing(df)
+    df = position_parsing(df)
+
+    columns_to_drop = ['Nationality', 'Unnamed: 7', 'Games', 'Pos']
+    df = drop_invalid_columns(df, columns_to_drop)
+    return df
+
 
 
 # Editions data cleaning
-
-df = pd.read_csv("../raw_data/editions.csv")
-
-df = df[df['Game_Type'] != 'Ancient Olympic Games']
-df.drop(columns = '#', inplace=True)
-df.rename(columns={'Unnamed: 7':'Comments'}, inplace=True)
-
-
 
 def normalize_competition(text):
     if pd.isna(text):
@@ -80,23 +140,99 @@ def normalize_competition(text):
         return f"{day_start} {month} - {day_end_part}"
     return text
 
-df["Competition"] = df["Competition"].apply(normalize_competition)
+
+def parse_competition(df):
+    df = df.copy()
+    df[["Competition_Start", "Competition_End"]] = (
+        df["Competition"]
+        .str.split("-", n=1, expand=True)  
+        .apply(lambda x: x.str.strip())
+    )
+    return df
 
 
-df[["Competition_Start", "Competition_End"]] = (
-    df["Competition"]
-    .str.split("-", n=1, expand=True)  
-    .apply(lambda x: x.str.strip())
-)
 
-date_cols = ["Opened", "Closed", "Competition_Start", "Competition_End"]
-for col in date_cols:
-    df[col] = pd.to_datetime(
-        df[col] + " " + df["Year"].astype(str),
-        format="%d %B %Y",
-        errors="coerce"
-    ).dt.strftime("%Y-%m-%d")
+def format_date(df, date_cols):
+    df = df.copy()
+    for col in date_cols:
+        df[col] = pd.to_datetime(
+            df[col] + " " + df["Year"].astype(str),
+            format="%d %B %Y",
+            errors="coerce"
+        ).dt.strftime("%Y-%m-%d")
+    return df
+    
+def remove_ancient_games(df):
+    df = df.copy()
+    df = df[df['Game_Type'] != 'Ancient Olympic Games']
+    return df
 
-df = df.drop(columns='Competition')
+def rename_comments_column(df):
+    df = df.copy()
+    df = df.rename(columns={'Unnamed: 7':'Comments'})
+    return df
 
-# df.to_csv('./clean_data/cleaned_editions.csv', index=False)
+
+def clean_editions(df):
+
+    df = df.copy()
+
+    df["Competition"] = df["Competition"].apply(normalize_competition)
+    df = parse_competition(df)
+
+    date_cols = ["Opened", "Closed", "Competition_Start", "Competition_End"]
+    df = format_date(df, date_cols)
+    df = remove_ancient_games(df)
+    df = rename_comments_column(df)
+
+    columns_to_drop = ['#', 'Competition']
+    df = drop_invalid_columns(df, columns_to_drop)
+
+    return df
+
+
+
+
+if __name__ == "__main__":
+
+    try:
+        os.makedirs("./clean_data", exist_ok=True)
+
+        bios_df = pd.read_csv("./raw_data/biodata.csv")
+        logger.info("Read biodata.csv")
+
+        results_df = pd.read_csv("./raw_data/results.csv")
+        logger.info("Read results.csv")
+
+        editions_df = pd.read_csv("./raw_data/editions.csv")
+        logger.info("Read editions.csv")
+
+
+
+        logger.info("Cleaning bios data")
+        bios_df = clean_biodata(bios_df)
+        logger.info(f"Biodata cleaned: {len(bios_df)} rows")
+
+        logger.info("Cleaning results data")
+        results_df = clean_results(results_df)
+        logger.info(f"Results cleaned: {len(results_df)} rows")
+
+        logger.info("Cleaning editions data")        
+        editions_df = clean_editions(editions_df)
+        logger.info(f"Editions cleaned: {len(editions_df)} rows")
+
+
+
+        bios_df.to_parquet('./clean_data/cleaned_biodata.parquet', index=False)
+        logger.info("Saved bios data")
+
+        results_df.to_parquet('./clean_data/cleaned_results.parquet', index=False)
+        logger.info("Saved results data")
+
+        editions_df.to_parquet('./clean_data/cleaned_editions.parquet', index=False)
+        logger.info("Saved editions data")
+
+    except Exception as e:
+        logger.exception(f"Error during cleaning pipeline: {e}")
+
+        
