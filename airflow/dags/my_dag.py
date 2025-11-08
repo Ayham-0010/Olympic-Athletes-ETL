8dde09030f77a472991ef59554d8dc5f3e189d67
Ayham <ayham.zinedine01@gmail.com>
2025-11-08 22:35:26 +0300

    creating airflow dag that runs the entire pipeline

--- airflow/dags/my_dag.py
+++ airflow/dags/my_dag.py
@@ -1,11 +1,15 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-
 from datetime import datetime, timedelta
 
-from src.data_clean import data_clean_I
 
+from src.data_clean import data_clean_I
+from src.data_clean_II import data_clean_II
+from src.data_quality_and_validation import data_validation_quality_checks
+from src.columns_renaming_reordering_and_final_save import column_rename_reorder
+from src.athlete_scrape import scrap_athletes
+from src.editions_scrap import scrap_editions
 
 default_args = {
 
@@ -19,7 +23,7 @@ default_args = {
 
 with DAG (
 
-    dag_id='Olympic_Athletes_ETL_DAG_V1',
+    dag_id='Olympic_Athletes_ETL',
     description='practice',
     default_args=default_args,
     start_date=datetime(2025,11,7),
@@ -28,10 +32,55 @@ with DAG (
 
 
 )as dag:
-    task1=PythonOperator(
+    
+
+    scrap_athletes_task=PythonOperator(
+
+    task_id = "scrap_athletes",
+    python_callable=scrap_athletes
+
+    )
+
+    scrap_editions_task=PythonOperator(
+
+    task_id = "scrap_editions",
+    python_callable=scrap_editions
+
+    )
+
+    data_clean_I_task=PythonOperator(
 
         task_id = "data_clean_I",
         python_callable=data_clean_I
 
     )
+    data_clean_II_task=PythonOperator(
+
+        task_id = "data_clean_II",
+        python_callable=data_clean_II
+
+    )
+
+    column_rename_reorder_task=PythonOperator(
+
+        task_id = "column_rename_reorder",
+        python_callable=column_rename_reorder
+
+    )
+
+    data_validation_quality_checks_task=PythonOperator(
+
+        task_id = "data_validation_quality_checks",
+        python_callable=data_validation_quality_checks
+
+    )
+
+
+scrap_athletes_task \
+>> scrap_editions_task \
+>> data_clean_I_task \
+>> data_clean_II_task \
+>> data_validation_quality_checks_task \
+>> column_rename_reorder_task
+
 
